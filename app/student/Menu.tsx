62d215ebd32dffaa32a5f5a9dfd4e0d2b7210ae3
shada <shadadaab@gmail.com>
2023-05-08 13:36:42 +0300

    finishing student pages

---
+++ app/student/Menu.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Link from "next/link";
+
+interface Item {
+  id: number;
+  name: string;
+  link: string;
+}
+
+const item: Item[] = [
+  { id: 1, link:"annoucments", name: "الإعلانات" },
+  { id: 2, link:"studentInfo",name: "بيانات الطالب" },
+  { id: 3, link:"courses",name: "المواد الدراسية" },
+  { id: 4, link:"courseProg",name: "جدول المحاضرات" },
+  { id: 5, link:"files",name: "الملفات" },
+  { id: 6, link:"examProg",name: "جدول الامتحانات" },
+  { id: 7, link:"examRes",name: "نتائج الامتحانات" },
+  { id: 8, link:"attendance",name: "الحضور" },
+  { id: 9, link:"courseReg",name: "تنزيل المواد" },
+];
+
+const Menu = () => {
+    const items = item.map((i) => (
+      <Link
+        
+        href={`/student/${i.link}`}
+        className="flex justify-between hover:text-lightBlue hover:text-md items-center p-1   "
+        key={i.id}
+      >
+        {i.name}
+      </Link>
+    ));
+    
+    return (
+      <div className="flex items-center justify-end pt-[100px]">
+        <h1 className="w-[232px] bg-darkBlue text-secondary text-sm flex flex-col h-[400px] justify-between items-center  ">
+          {items}
+        </h1>
+      </div>
+    );
+
+}
+
+export default Menu
