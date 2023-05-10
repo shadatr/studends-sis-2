7375e93267d16da2456717323c4b6984680211f2
shada <shadadaab@gmail.com>
2023-05-10 12:12:19 +0300

    .

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import Link from "next/link";
 
 interface Item {
@@ -8,39 +8,37 @@ interface Item {
 }
 
 const item: Item[] = [
-  { id: 1, link: "announcments", name: "الإعلانات" },
-  { id: 2, link: "studentInfo", name: "بيانات الطالب" },
-  { id: 3, link: "courses", name: "المواد الدراسية" },
-  { id: 4, link: "courseProg", name: "جدول المحاضرات" },
-  { id: 5, link: "files", name: "الملفات" },
-  { id: 6, link: "examProg", name: "جدول الامتحانات" },
-  { id: 7, link: "examRes", name: "نتائج الامتحانات" },
-  { id: 8, link: "attendance", name: "الحضور" },
-  { id: 9, link: "courseReg", name: "تنزيل المواد" },
+  { id: 1, link:"annoucments", name: "الإعلانات" },
+  { id: 2, link:"studentInfo",name: "بيانات الطالب" },
+  { id: 3, link:"courses",name: "المواد الدراسية" },
+  { id: 4, link:"courseProg",name: "جدول المحاضرات" },
+  { id: 5, link:"files",name: "الملفات" },
+  { id: 6, link:"examProg",name: "جدول الامتحانات" },
+  { id: 7, link:"examRes",name: "نتائج الامتحانات" },
+  { id: 8, link:"attendance",name: "الحضور" },
+  { id: 9, link:"courseReg",name: "تنزيل المواد" },
 ];
 
-function Menu() {
-  const data = item.map((i) => (
-    <tr key={i.id}>
-      <td className="flex p-2 items-center justify-center hover:text-[23px] hover:text-cyan-300">
-        <Link
-          href={`/student/${i.link}`}
-          className="flex justify-between items-center p-1   "
-          key={i.id}
-        >
-          {i.name}
-        </Link>
-      </td>
-    </tr>
-  ));
+const Menu = () => {
+    const items = item.map((i) => (
+      <Link
+        
+        href={`/student/${i.link}`}
+        className="flex justify-between hover:text-lightBlue hover:text-md items-center p-1   "
+        key={i.id}
+      >
+        {i.name}
+      </Link>
+    ));
+    
+    return (
+      <div className="flex items-center justify-end pt-[100px]">
+        <h1 className="w-[232px] bg-darkBlue text-secondary text-sm flex flex-col h-[400px] justify-between items-center  ">
+          {items}
+        </h1>
+      </div>
+    );
 
-  return (
-    <div className="  flex justify-end  text-sm ">
-      <table className=" text-sm flex flex-col mt-[150px] w-[260px] bg-darkBlue text-white ">
-        {data}
-      </table>
-    </div>
-  );
 }
 
-export default Menu;
+export default Menu
