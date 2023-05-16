cd14a95bcb957730f0945482f671320f672878d0
AD DAB <adamtareq138@gmail.com>
2023-05-16 03:44:05 +0300

    some changes to the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,21 @@
-"use client";
-import { useSession, signOut } from "next-auth/react";
-import React from "react";
+'use client';
+import { useSession, signOut } from 'next-auth/react';
+import React from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiUser } from 'react-icons/bi';
 
 const Navbar = () => {
-  // const session = useSession()
+  const session = useSession({ required: true });
   return (
     <div className="flex justify-between text-lg text-left p-[35px] bg-darkBlue text-secondary">
       <p>جامعة طرابلس</p>
-      <button onClick={() => (signOut({redirect : true, callbackUrl : "/"}))} >logout</button>
+      <div className='flex items-center'>
+      <BiUser className='h-6 hover:cursor-pointer'/>
+        <p className='text-[20px]'>{session.data?.user.name} {session.data?.user.surname}</p>
+      <button onClick={() => signOut({ redirect: true})}>
+        < AiOutlineLogout className='h-6'/>
+      </button>
+      </div>
     </div>
   );
 };
