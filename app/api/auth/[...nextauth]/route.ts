6a38ced1a532c8655f793212a241ee7e24fb414e
AD DAB <adamtareq138@gmail.com>
2023-05-16 03:23:33 +0300

    added admin login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,8 +9,39 @@ import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(process.env.SUPABASE_URL || "", process.env.SUPABASE_KEY || "");
 
 const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
+  providers: [CredentialsProvider({
+    name: "admin",
+    id: 'admin',
+    credentials: {
+      email: {
+        label: "البريد الالكتروني",
+        type: "text",
+        placeholder: "email@example.com",
+      },
+      password: {
+        label: "كلمة المرور",
+        type: "password",
+      },
+    },
+    async authorize(credentials, req) {
+      const { email, password } = credentials as any;
+      const passwordHash = createHash('sha256').update(password).digest('hex');
+
+      const { data, error } = await supabase
+        .from('tb_admins')
+        .select('*')
+        .eq('email', email)
+        .eq('password', passwordHash);
+
+      if (!data && error || data && data.length === 0) {
+
+        return null;
+      } else {
+
+        return data[0] as any;
+      }
+    },
+  }),
     CredentialsProvider({
       name: "professor",
       id: 'professor',
@@ -45,7 +76,6 @@ const authOptions: NextAuthOptions = {
       },
     }),
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Student",
       id: 'student',
 
