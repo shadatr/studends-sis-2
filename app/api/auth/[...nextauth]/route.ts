e5d2ce6287c76ee34484123e800c626bde0c3f6d
AD DAB <adamtareq138@gmail.com>
2023-05-16 03:20:34 +0300

    added professor login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,7 +11,39 @@ const supabase = createClient(process.env.SUPABASE_URL || "", process.env.SUPABA
 const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
-    // ...add more providers here
+    CredentialsProvider({
+      name: "professor",
+      id: 'professor',
+      credentials: {
+        email: {
+          label: "البريد الالكتروني",
+          type: "text",
+          placeholder: "email@example.com",
+        },
+        password: {
+          label: "كلمة المرور",
+          type: "password",
+        },
+      },
+      async authorize(credentials, req) {
+        const { email, password } = credentials as any;
+        const passwordHash = createHash('sha256').update(password).digest('hex');
+
+        const { data, error } = await supabase
+          .from('tb_doctors')
+          .select('*')
+          .eq('email', email)
+          .eq('password', passwordHash);
+
+        if (!data && error || data && data.length === 0) {
+
+          return null;
+        } else {
+
+          return data[0] as any;
+        }
+      },
+    }),
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Student",
