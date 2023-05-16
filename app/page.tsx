40b5b67a09f2dbf11969b024703d17ca5b2b6c54
AD DAB <adamtareq138@gmail.com>
2023-05-16 03:37:32 +0300

    a lil tweak

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,14 @@
 import { signOut, useSession } from 'next-auth/react';
 
 export default function Home() {
-  useSession({ required: true });
+  const session = useSession({ required: true });
 
   return (
     <div className='flex justify-center items-center h-full flex-col'>
+      <h1 className='text-3xl'>Welcome {session.data?.user.name}</h1>
       <p className='text-lg'>This page should be private</p>
       <p className='text-red-600 text-sm hover:cursor-pointer' onClick={() => signOut()}>logout</p>
     </div>
   );
 }
 
-interface UserSwitchProps {
-  student: boolean;
-  setStudent: (student: boolean) => void;
-}
