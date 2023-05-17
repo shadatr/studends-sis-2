bfcc934c0a610e8139f97fee22cd87f47ca41c83
AD DAB <adamtareq138@gmail.com>
2023-05-17 19:08:31 +0300

    added some UX improvements

--- app/management/majorStudents/[name]/page.tsx
+++ app/management/majorStudents/[name]/page.tsx
@@ -6,8 +6,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const page = ({ params }: { params: { name: string } }) => {
-
-    const [students, setStudents] = useState<PersonalInfoType[]>([]);
+  const [students, setStudents] = useState<PersonalInfoType[]>([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -21,10 +20,13 @@ const page = ({ params }: { params: { name: string } }) => {
   }, [params.name]);
 
   const studentsList = students.map((item, index) => (
-    <table key={index} className="flex absolute text-sm justify-center items-center mt-10 left-[200px]">
+    <table
+      key={index}
+      className="flex absolute text-sm justify-center items-center mt-10 left-[200px]"
+    >
       <tr className="flex flex-row">
         <td className="flex flex-row w-[800px] p-1 items-center justify-end">
-          {item.surname} { }
+          {item.surname} {}
           {item.name}
         </td>
         <td className="flex flex-row w-1/7 pr-2 pl-2">{index + 1}</td>
@@ -32,7 +34,14 @@ const page = ({ params }: { params: { name: string } }) => {
     </table>
   ));
 
-  return <div>{studentsList}</div>;
+  const noStudents = (
+    <div className='h-full flex justify-center items-center w-full'>
+      <h1 className="flex flex-row justify-center items-center text-2xl">
+        لا يوجد طلاب
+      </h1>
+    </div>
+  );
+  return <div>{students.length == 0 ? noStudents : studentsList}</div>;
 };
 
 export default page;
