7a5f83ad11d9617aec784728fbfba6fc36eae109
AD DAB <adamtareq138@gmail.com>
2023-05-17 19:31:27 +0300

    make the modal open and close faster

--- app/components/dialog.jsx
+++ app/components/dialog.jsx
@@ -24,7 +24,7 @@ export default function MyModal({ deleteModle, name, depOrMaj }) {
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
           <Transition.Child
             as={Fragment}
-            enter="ease-out duration-300"
+            enter="ease-out duration-200"
             enterFrom="opacity-0"
             enterTo="opacity-100"
             leave="ease-in duration-200"
@@ -38,7 +38,7 @@ export default function MyModal({ deleteModle, name, depOrMaj }) {
             <div className="flex min-h-full items-center justify-center p-4 text-center">
               <Transition.Child
                 as={Fragment}
-                enter="ease-out duration-300"
+                enter="ease-out duration-200"
                 enterFrom="opacity-0 scale-95"
                 enterTo="opacity-100 scale-100"
                 leave="ease-in duration-200"
