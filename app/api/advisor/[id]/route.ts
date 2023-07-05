430bd726c139c89c751303dd10896c2eb474607a
shada <shadadaab@gmail.com>
2023-07-05 21:15:18 +0300

    exam res

---
+++ app/api/advisor/[id]/route.ts
@@ -0,0 +1,50 @@
+import { createClient } from '@supabase/supabase-js';
+import { AssignAdvisorType } from '@/app/types/types';
+
+const supabase = createClient(
+  process.env.SUPABASE_URL || '',
+  process.env.SUPABASE_KEY || ''
+);
+
+export async function POST(request: Request) {
+  const data: AssignAdvisorType = await request.json();
+  console.log(data);
+
+  try {
+    const res = await supabase.from('tb_students').update( {advisor: data.advisor} ).eq("id", data.id);
+    console.log(res);
+    
+    return new Response(
+      JSON.stringify({ message: 'تم تعيين الدكتور بنجاح ' }),
+      {
+        headers: { 'content-type': 'application/json' },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({ message: 'حدث خطأ اثناء  تعيين الدكتور ' }),
+      { headers: { 'content-type': 'application/json' }, status: 400 }
+    );
+  }
+}
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: number } }
+) {
+  try {
+    const data = await supabase
+      .from('tb_students')
+      .select('*')
+      .eq('advisor', params.id);
+
+    if (data.error) {
+      return new Response(JSON.stringify({ message: 'an error occured' }), {
+        status: 403,
+      });
+    }
+
+    return new Response(JSON.stringify({ message: data.data }));
+  } catch {}
+}
