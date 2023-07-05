5e0ed586120eccac9f16f3785640779b0b1f9ea5
shada <shadadaab@gmail.com>
2023-07-05 12:20:51 +0300

    new

--- app/api/getAll/getAllDepartments/route.ts
+++ app/api/getAll/getAllDepartments/route.ts
@@ -20,6 +20,6 @@ export async function GET(
       });
     }
 
-    return new Response(JSON.stringify({ message: data }));
+    return new Response(JSON.stringify({ message: data.data }));
   } catch {}
 }
