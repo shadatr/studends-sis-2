fe224ade66a240eb4c73b469c095895be83fa630
shada <shadadaab@gmail.com>
2023-07-05 11:46:20 +0300

    student majors route edited

--- app/api/major/majorStudents/[id]/route.ts
+++ app/api/major/majorStudents/[id]/route.ts
@@ -1,26 +1,28 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.SUPABASE_URL || '',
-  process.env.SUPABASE_KEY || ''
-);
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseKey = process.env.SUPABASE_KEY || '';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function GET(
   request: Request,
   { params }: { params: { id: number } }
 ) {
   try {
-    const data = await supabase
+    const { data, error } = await supabase
       .from('tb_students')
       .select('*')
       .eq('major', params.id);
 
-    if (data.error) {
-      return new Response(JSON.stringify({ message: 'an error occured' }), {
+    if (error) {
+      return new Response(JSON.stringify({ message: 'an error occurred' }), {
         status: 403,
       });
     }
 
-    return new Response(JSON.stringify({ message: data.data }));
-  } catch {}
+    return new Response(JSON.stringify({ message: data }));
+  } catch (error) {
+    // Handle the error here if necessary
+  }
 }
