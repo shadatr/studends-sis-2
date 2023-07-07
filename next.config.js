5a6893b72f34ca8a6d6c7523742daa0b1b50ad9c
shada <shadadaab@gmail.com>
2023-07-07 10:48:39 +0300

    removing header, again

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
