d3f18198e16459664a419404fbc8d775cc61b527
shdab <130516923+shadatr@users.noreply.github.com>
2023-07-24 14:48:42 +0300

    error fixed

--- app/api/getAll/majStudents/[id]/route.ts
+++ app/api/getAll/majStudents/[id]/route.ts
@@ -1,29 +1,37 @@
-import { createClient } from '@supabase/supabase-js';
+import { Client } from 'pg';
 
-const supabase = createClient(
-  process.env.SUPABASE_URL || '',
-  process.env.SUPABASE_KEY || ''
-);
+const bg = new Client({
+  user: process.env.DB_USERNAME || '',
+  password: process.env.DB_PASSWORD || '',
+  host: process.env.DB_HOST || '',
+  database: process.env.DB_NAME || '',
+  port: Number(process.env.DB_PORT),
+});
 
 export async function GET(
   request: Request,
   { params }: { params: { id: number } }
 ) {
   try {
+    await bg.connect();
 
-    const data = await supabase
-      .from('tb_students')
-      .select('*')
-      .eq('major', params.id).eq('graduated', false);
+    const queryResult = await bg.query(
+      'SELECT * FROM tb_students WHERE major = $1 AND graduated = false',
+      [params.id]
+    );
 
-    if (data.error) {
-      return new Response(JSON.stringify({ message: 'an error occured' }), {
-        status: 403
-      });
-    }
+    const data = queryResult.rows;
 
-    return new Response(JSON.stringify({ message: data.data }),);
-  } catch {
-    // Handle error
+    await bg.end();
+
+    return new Response(JSON.stringify({ message: data }), {
+      headers: { 'content-type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('Error occurred:', error);
+    await bg.end();
+    return new Response(JSON.stringify({ message: 'an error occurred' }), {
+      status: 403,
+    });
   }
 }
