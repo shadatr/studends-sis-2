38f493b327855cbd99e25fdb04badcfc579434ef
shdab <130516923+shadatr@users.noreply.github.com>
2023-07-26 18:33:09 +0300

    force-dynami

--- app/api/getAll/getAllStaff/route.ts
+++ app/api/getAll/getAllStaff/route.ts
@@ -1,19 +1,26 @@
-import { Database } from "@/app/types/supabase";
+import { Database } from '@/app/types/supabase';
 import { AdminStaffType } from '@/app/types/types';
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient<Database>(process.env.SUPABASE_URL || "", process.env.SUPABASE_KEY || "");
-
+const supabase = createClient<Database>(
+  process.env.SUPABASE_URL || '',
+  process.env.SUPABASE_KEY || ''
+);
 
 export async function GET() {
-
-  const result = await supabase.from("tb_admins").select("*");
+  const result = await supabase.from('tb_admins').select('*');
   const resp: AdminStaffType[] = [];
 
   if (result.error) {
-    return { status: 400, body: { message: "there is an error" } };
+    return { status: 400, body: { message: 'there is an error' } };
   }
 
-  return new Response(JSON.stringify({ message: resp }), { status: 200 });
+  const response = new Response(JSON.stringify({ message: resp }), {
+    status: 200,
+  });
+
+  // Set the revalidate option to "force-dynamic"
+  (response as any).revalidate = 'force-dynamic';
+
+  return response;
 }
-export const revalidate = false;
