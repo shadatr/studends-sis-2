a4a813acf88bdc62e2bf56b9544f59873bebb64f
shdab <130516923+shadatr@users.noreply.github.com>
2023-07-27 20:40:54 +0300

    exam results

--- app/api/exams/examRes/[id]/[name]/route.ts
+++ app/api/exams/examRes/[id]/[name]/route.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import {  StudentClassType } from '@/app/types/types';
+import { StudentClassType } from '@/app/types/types';
 
 const supabase = createClient(
   process.env.SUPABASE_URL || '',
@@ -25,32 +25,32 @@ export async function GET(
       .eq('id', params.id);
 
     if (classes && sections) {
-      
       const { data: courseEnrollments } = await supabase
         .from('tb_course_enrollment')
         .select('*')
         .eq('approved', true)
-        .eq('class_id', classes[0].id).order('id', { ascending: false });
-  
+        .eq('class_id', classes[0].id)
+        .order('id', { ascending: false });
+
       const { data: course } = await supabase
         .from('tb_courses')
         .select('*')
         .eq('id', sections[0].course_id);
-  
-        
-        const data = {
-          courseEnrollements: courseEnrollments,
-          course: course,
-          section: sections,
-          class: classes
-        };
-  
+
+      const data = {
+        courseEnrollements: courseEnrollments,
+        course: course,
+        section: sections,
+        class: classes,
+      };
+
       return new Response(JSON.stringify({ message: data }), {
         status: 200,
         headers: { revalidate: dynamic },
       });
-    }else{ console.log("not found");}
-
+    } else {
+      console.log('not found');
+    }
   } catch (error) {
     return new Response(JSON.stringify({ message: 'An error occurred' }), {
       status: 500,
@@ -58,13 +58,10 @@ export async function GET(
   }
 }
 
-export async function POST(
-  request: Request
-) {
-
+export async function POST(request: Request) {
   const data1 = await request.json();
 
-  const res=await Promise.all(
+  const res = await Promise.all(
     data1.map(async (item: StudentClassType) => {
       const data = await supabase
         .from('tb_course_enrollment')
